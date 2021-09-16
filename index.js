a2f68b981bc9e4f1ec2ce4102c4753f485830f41
Jose <J_Lanza.13@live.com>
2021-09-16 11:47:49 -0400

    Minor html edit and completed form submisson

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ fetch(BASE_URL)
     trickArr.forEach(trickObj => renderTricks(trickObj))
 });
 
-const skateImgs = document.querySelector('div.skate-images');
-//skateImgs.addEventListener('click', dispHandler);
+const trickMenu = document.querySelector('div.skate-images');
 
 const trickDets = document.querySelector('div#trick-details');
 
 const trickForm = document.querySelector('form.trick-form');
-//trickForm.addEventlistener('submit', createTrick)
+trickForm.addEventListener('submit', formInfo)
 
 function renderTricks(trick) {
     const trickName = document.createElement('h2')
     trickName.textContent = trick.name
     trickName.addEventListener('click', event => getTrick(event, trick))
 
-    skateImgs.append(trickName)
+    trickMenu.append(trickName)
 }
 
 function getTrick(event, trick) {
     const trickH2 = document.querySelector('.skate-trick');
-    trickH2.textContent = trick.name; // undefined
+    trickH2.textContent = trick.name;
     
     const imgDisp = document.querySelector('.skate-img');
     imgDisp.src = trick.image;
     imgDisp.alt = trick.name;
+
+    const trickInst = document.querySelector('#instruction')
+    trickInst.textContent = `Instruction: ${trick.instruction}`
 
     const vidLink = document.querySelector('#vid-link');
     vidLink.textContent = `Vid: ${trick.video}`;
@@ -40,11 +42,31 @@ function getTrick(event, trick) {
     dopeCount.textContent = `Dope: ${trick.likes}`;
     
     const dopeBttn = document.querySelector('#dope-counter')
-    dopeBttn.addEventListener('click', () => dopeCounter(trick, dopeCount))
+    dopeBttn.addEventlistener('click', () => dopeCounter(trick, dopeCount))
 
 }
 
 function dopeCounter(trick, dopeCount) {
     ++trick.likes;
-    dopeCount.textContent = trick.likes;
+    dopeCount.textContent = `Dope: ${trick.likes}`;
+}
+
+function formInfo(event) {
+    console.log(event)
+    event.preventDefault();
+
+    const newTrick = {
+        name: event.target['name'].value,
+        image: event.target['img'].value,
+        difficulty: event.target['diff'].value,
+        instruction: event.target['inst'].value,
+        video: event.target['vid'].value,
+        likes: 0,
+
+    }
+
+    renderTricks(newTrick)
+
+    event.target.reset()
+
 }
